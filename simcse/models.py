4d9583dafd2f510ccb0d47940e0d5ac705dfe6fe
QiwenZz <q2zhang@ucsd.edu>
2023-12-05 14:42:00 -0500

    changed distance metric from dot product to cosine similarity

--- simcse/models.py
+++ simcse/models.py
@@ -118,6 +118,33 @@ def max_distance_pairs(tensor):
 
     return max_pairs_tensor
 
+# Function to calculate pairwise cosine similarities
+def pairwise_cosine_similarities(batch):
+    # Normalize the batch to unit vectors along the last dimension
+    normalized_batch = F.normalize(batch, p=2, dim=1)
+    # Compute cosine similarity for each pair
+    num_candidates = normalized_batch.shape[0]
+    similarity_matrix = torch.zeros((num_candidates, num_candidates))
+    for i in range(num_candidates):
+        for j in range(num_candidates):
+            similarity_matrix[i, j] = F.cosine_similarity(normalized_batch[i], normalized_batch[j], dim=0)
+    return similarity_matrix
+
+# Function to find the tensor of pairs with the minimum cosine similarity (maximum distance)
+def min_cosine_similarity_pairs(tensor):
+    max_pairs_tensor = torch.empty(tensor.size(0), 2, tensor.size(2))
+
+    for i, batch in enumerate(tensor):
+        similarities = pairwise_cosine_similarities(batch)
+        # Fill diagonal with a high value to ignore self-similarities
+        similarities.fill_diagonal_(2)
+        # Find the indices of the minimum cosine similarity
+        min_idx = torch.argmin(similarities)
+        min_pair_indices = (min_idx // similarities.size(1), min_idx % similarities.size(1))
+        max_pairs_tensor[i] = torch.stack((batch[min_pair_indices[0]], batch[min_pair_indices[1]]))
+
+    return max_pairs_tensor
+
 def cl_forward(cls,
     encoder,
     input_ids=None,
@@ -201,8 +228,13 @@ def cl_forward(cls,
 
 
     # Apply the function to the entire tensor
-    max_pairs_tensor = max_distance_pairs(pooler_output)
-    z1, z2 = max_pairs_tensor[:,0].cuda(), max_pairs_tensor[:,1].cuda()
+    min_pairs_tensor = min_cosine_similarity_pairs(pooler_output)
+
+    print('max_pairs_tensor', min_pairs_tensor)
+
+
+
+    z1, z2 = min_pairs_tensor[:,0].cuda(), min_pairs_tensor[:,1].cuda()
     print('z1 device',z1.get_device())
 
 
--- test_pairwise.py
+++ test_pairwise.py
@@ -1,43 +1,54 @@
 import torch
+import torch.nn.functional as F
 
 # 4x 5 x3
 tensor = torch.tensor([
-    [[1, 2, 3], [2, 3, 4], [3, 4, 5], [6, 7, 8], [9, 10, 11]],  # Batch 1
-    [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]],    # Batch 2
-    [[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1]],    # Batch 3
-    [[9, 8, 7], [8, 7, 6], [7, 6, 5], [6, 5, 4], [5, 4, 3]]     # Batch 4
+    # Batch 1: Distinct directions
+    [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0], [0, 1, 1]],
+    # Batch 2: Varying magnitudes and directions
+    [[1, 2, 3], [4, 5, 6], [10, 0, 0], [0, 10, 0], [0, 0, 10]],
+    # Batch 3: Negative and positive values
+    [[-1, -1, 0], [1, 1, 0], [1, -1, 0], [0, 1, -1], [0, -1, 1]],
+    # Batch 4: Mixed values
+    [[1, 2, -2], [-1, -2, 2], [3, -3, 1], [-3, 3, -1], [2, -2, 3]]
 ], dtype=torch.float32)
 
-# Function to calculate pairwise distances
-def pairwise_distances(batch):
-    # Compute pairwise distance matrix
-    distance_matrix = torch.cdist(batch, batch, p=2)
-    return distance_matrix
+# Function to calculate pairwise cosine similarities
+def pairwise_cosine_similarities(batch):
+    # Normalize the batch to unit vectors along the last dimension
+    normalized_batch = F.normalize(batch, p=2, dim=1)
+    # Compute cosine similarity for each pair
+    num_candidates = normalized_batch.shape[0]
+    similarity_matrix = torch.zeros((num_candidates, num_candidates))
+    for i in range(num_candidates):
+        for j in range(num_candidates):
+            similarity_matrix[i, j] = F.cosine_similarity(normalized_batch[i], normalized_batch[j], dim=0)
+    return similarity_matrix
 
-# Function to find the tensor of pairs with the maximum distance
-def max_distance_pairs(tensor):
+# Function to find the tensor of pairs with the minimum cosine similarity (maximum distance)
+def min_cosine_similarity_pairs(tensor):
     max_pairs_tensor = torch.empty(tensor.size(0), 2, tensor.size(2))
 
     for i, batch in enumerate(tensor):
-        distances = pairwise_distances(batch)
-        # Fill diagonal with -inf to ignore self-distances
-        distances.fill_diagonal_(-float('inf'))
-        # Find the indices of the maximum distance
-        max_idx = torch.argmax(distances)
-        max_pair_indices = (max_idx // distances.size(1), max_idx % distances.size(1))
-        max_pairs_tensor[i] = torch.stack((batch[max_pair_indices[0]], batch[max_pair_indices[1]]))
+        similarities = pairwise_cosine_similarities(batch)
+        # Fill diagonal with a high value to ignore self-similarities
+        similarities.fill_diagonal_(2)
+        # Find the indices of the minimum cosine similarity
+        min_idx = torch.argmin(similarities)
+        min_pair_indices = (min_idx // similarities.size(1), min_idx % similarities.size(1))
+        max_pairs_tensor[i] = torch.stack((batch[min_pair_indices[0]], batch[min_pair_indices[1]]))
 
     return max_pairs_tensor
 
 # Apply the function to the entire tensor
-max_pairs_tensor = max_distance_pairs(tensor)
+min_pairs_tensor = min_cosine_similarity_pairs(tensor)
 
-print('max_pairs_tensor',max_pairs_tensor)
+print('max_pairs_tensor', min_pairs_tensor)
 
 
 
-z1, z2 = max_pairs_tensor[:,0], max_pairs_tensor[:,1]
+z1, z2 = min_pairs_tensor[:,0], min_pairs_tensor[:,1]
 
-print(max_pairs_tensor.shape)
+print(min_pairs_tensor.shape)
 print(z1)
 print(z2)
